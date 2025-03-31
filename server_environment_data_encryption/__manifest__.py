7f470fbb5c43c3427519faf447092a48b1305e98
Florian da Costa <florian.dacosta@akretion.com>
2025-03-31 17:23:30 +0200

    [MIG] Migrate server_environment_data_encryption to 18

--- server_environment_data_encryption/__manifest__.py
+++ server_environment_data_encryption/__manifest__.py
@@ -1,8 +1,9 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Server Environment Data Encryption",
-    "version": "16.0.1.0.0",
-    "development_status": "Alpha",
+    "version": "18.0.1.0.0",
+    "development_status": "Production/Stable",
+    "maintainers": ["florian-dacosta"],
     "category": "Tools",
     "website": "https://github.com/OCA/server-env",
     "author": "Akretion, Odoo Community Association (OCA)",
--- server_environment_data_encryption/tests/test_server_environment_data_encrypt.py
+++ server_environment_data_encryption/tests/test_server_environment_data_encrypt.py
@@ -8,8 +8,9 @@ from odoo.addons.data_encryption.tests.common import CommonDataEncrypted
 
 
 class TestServerEnvDataEncrypted(CommonDataEncrypted):
-    def test_dynamic_view_current_env(self):
+    def test_view_with_env_update_button(self):
         self.maxDiff = None
+        # common class already set test environment (as default)
         self.set_new_key_env("prod")
         self.set_new_key_env("preprod")
         mixin_obj = self.env["server.env.mixin"]
@@ -17,31 +18,60 @@ class TestServerEnvDataEncrypted(CommonDataEncrypted):
         xml_str = base_path.read_text()
         xml = etree.XML(xml_str)
         res_xml = mixin_obj._update_form_view_from_env(xml, "form")
-        expected_xml_path = Path(__file__).parent / "fixtures" / "res1.xml"
-        expected_xml = expected_xml_path.read_text()
-        # convert both to xml with parser removing space then convert to string to
-        # compare
-        parser = etree.XMLParser(remove_blank_text=True)
-        res_xml_str = etree.tostring(etree.XML(etree.tostring(res_xml), parser=parser))
-        expected_xml_str = etree.tostring(etree.XML(expected_xml, parser=parser))
-        self.assertEqual(res_xml_str, expected_xml_str)
 
-    def test_dynamic_view_other_env(self):
-        self.maxDiff = None
-        self.set_new_key_env("prod")
-        self.set_new_key_env("preprod")
-        mixin_obj = self.env["server.env.mixin"]
-        base_path = Path(__file__).parent / "fixtures" / "base.xml"
-        xml_str = base_path.read_text()
-        xml = etree.XML(xml_str)
-        res_xml = mixin_obj.with_context(environment="prod")._update_form_view_from_env(
-            xml, "form"
+        # check we have 3 alert div for our 3 environments
+        env_div_xml = res_xml.find("sheet").find("div").findall("div")
+        # 3 alert div + 1 button div
+        self.assertEqual(len(env_div_xml), 4)
+        # test first alert test div
+        test_alert_div = env_div_xml[0]
+        self.assertEqual(
+            test_alert_div.get("invisible"),
+            "context.get('environment', 'test') != 'test'",
         )
-        expected_xml_path = Path(__file__).parent / "fixtures" / "res2.xml"
-        expected_xml = expected_xml_path.read_text()
-        # convert both to xml with parser removing space then convert to string to
-        # compare
-        parser = etree.XMLParser(remove_blank_text=True)
-        res_xml_str = etree.tostring(etree.XML(etree.tostring(res_xml), parser=parser))
-        expected_xml_str = etree.tostring(etree.XML(expected_xml, parser=parser))
-        self.assertEqual(res_xml_str, expected_xml_str)
+        self.assertEqual(
+            test_alert_div.find("strong").text, "Modify values for test environment"
+        )
+        # test preprod div
+        preprod_alert_div = env_div_xml[1]
+        self.assertEqual(
+            preprod_alert_div.get("invisible"),
+            "context.get('environment', 'test') != 'preprod'",
+        )
+        self.assertEqual(
+            preprod_alert_div.find("strong").text,
+            "Modify values for preprod environment",
+        )
+        # test buttons
+        button_div = env_div_xml[-1]
+        # 3 buttons for 3 env
+        self.assertEqual(len(button_div.findall("button")), 3)
+        test_button = button_div.findall("button")[0]
+        # test env button
+        self.assertEqual(
+            test_button.get("invisible"), "context.get('environment', 'test') == 'test'"
+        )
+        self.assertEqual(
+            test_button.get("name"), "action_change_env_data_encrypted_fields"
+        )
+        self.assertEqual(test_button.get("string"), "Define values for test")
+        # preprod button
+        preprod_button = button_div.findall("button")[1]
+        self.assertEqual(
+            preprod_button.get("invisible"),
+            "context.get('environment', 'test') == 'preprod'",
+        )
+        self.assertEqual(preprod_button.get("string"), "Define values for preprod")
+        self.assertEqual(preprod_button.get("context"), "{'environment': 'preprod'}")
+        # test normal field with pre-existing readonly condition
+        test2_field = res_xml.find("sheet").findall(".//field[@name='test2']")[0]
+        self.assertEqual(
+            test2_field.get("readonly"),
+            "(not type_env_is_editable) or context.get(\"environment\", 'test') != "
+            "'test'",
+        )
+        # test normal field without pre-existing readonly condition
+        test_field = res_xml.find("sheet").findall(".//field[@name='test']")[0]
+        self.assertEqual(
+            test_field.get("readonly"), "context.get(\"environment\", 'test') != 'test'"
+        )
